9252d1d644abd63dbd714679a075595d5d67afdd
EthanK2000 <ethkraus@gmail.com>
2024-08-11 22:01:53 +0100

    v0

--- app/main.py
+++ app/main.py
@@ -1,47 +1,132 @@
 import streamlit as st
 import requests
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 import dotenv
 import os
-from streamlit import runtime
-from streamlit.runtime.scriptrunner import get_script_run_ctx
+import time
 
 dotenv.load_dotenv()
 
-def get_remote_ip() -> str:
-    try:
-        ctx = get_script_run_ctx()
-        if ctx is None:
-            return None
-        session_info = runtime.get_instance().get_client(ctx.session_id)
-        if session_info is None:
-            return None
-    except Exception as e:
-        return None
-    return session_info.request.remote_ip
+st.set_page_config(page_title='VisaInfo.chat', page_icon='app/VisaInfo-chat-logo.png')
 
-st.title("VisaInfo.chat")
+st.markdown("""
+<style>
+    [data-testid="stDecoration"] {
+        background-image: none;
+        background-color: rgb(255, 189, 69);
+    }
+    [data-testid="stButtonGroup"] {
+        justify-content: center;
+    }
+</style>
+""", unsafe_allow_html=True)
 
-user_id = "IP-" + get_remote_ip()
-client = MongoClient(os.environ['MONGODB_ATLAS_URI'], server_api=ServerApi('1'))
-database = client["VisaInfo-Database"]
-collection = database["User"]
-query_filter = {'user_id': user_id}
-user = collection.find_one(query_filter)
+if 'countryData' not in st.session_state:
+    res = requests.get(os.environ["VISAINFO_ENDPOINT"]+"getcountries")
+    countryData = res.json()
+    st.session_state['countryData'] = requests.get(os.environ["VISAINFO_ENDPOINT"]+"getcountries").json()
 
-if not user:
-    user = query_filter
-    user["messages"] = [{"role": "assistant", "content": "*🛫 Welcome to VisaInfo.chat*\n\nI'm here to assist you in quickly finding out information about the visa you need.\nWhere do you plan on traveling to?\n\nT&Cs -> https://visainfo.chat/tcs\nFeedback -> https://visainfo.chat/fdb"}]
-    collection.insert_one(user)
+def restart():
+    for key in st.session_state.keys():
+        del st.session_state[key]
 
-for msg in user["messages"]:
-    st.chat_message(msg["role"]).write(msg["content"])
+def generate_visa_info():
+    st.markdown("""
+    <style>
+        [data-testid="baseButton-primary"] {
+            visibility: hidden;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+    st.session_state['state'] = 1
+    generic_message = f"I am travelling to {st.session_state['destination']} with a {st.session_state['passport']} passport, do I need to get a visa?"
+    st.session_state['user']["messages"].append({"role": "user", "content": generic_message})
+    res = requests.post(os.environ["VISAINFO_ENDPOINT"]+"queryvisainfo", json = {
+        "query": generic_message,
+        "destination": st.session_state['destination'],
+        "passport": st.session_state['passport'],
+        "specific": False,
+        "sess_id": st.session_state['user']["user_id"]
+    })
+    response = res.json()
+    st.session_state['user']["messages"].append({"role": "assistant", "content": response["response"], "feedback_id": response["feedback_id"]})
 
-if prompt := st.chat_input(placeholder="What visa information would you like to know?"):
-    user["messages"].append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    msg = get_remote_ip()
-    user["messages"].append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
-    collection.replace_one(query_filter,  user)
+def prompt_visa_info():
+    st.session_state['user']["messages"].append({"role": "user", "content": st.session_state['prompt']})
+    st.chat_message("user").write(st.session_state['prompt'])
+    res = requests.post(os.environ["VISAINFO_ENDPOINT"]+"queryvisainfo", json = {
+        "query": st.session_state['prompt'],
+        "destination": st.session_state['destination'],
+        "passport": st.session_state['passport'],
+        "specific": True,
+        "sess_id": st.session_state['user']["user_id"]
+    })
+    msg = res.json()["response"]
+    st.session_state['user']["messages"].append({"role": "assistant", "content": msg})
+
+if 'user' not in st.session_state:
+    st.session_state['user'] = requests.get(os.environ["VISAINFO_ENDPOINT"]+"getuser").json()
+if 'state' not in st.session_state:
+    st.session_state['state'] = 0
+if 'prompt' not in st.session_state:
+    st.session_state['prompt'] = None
+
+if st.session_state['state'] > 0:
+    st.button("Restart", on_click=restart)
+
+for idx, msg in enumerate(st.session_state['user']["messages"]):
+    if idx == 0:
+        st.chat_message(msg["role"], avatar="app/VisaInfo-chat-logo.png").write(msg["content"])
+
+if st.session_state['state'] == 1:
+    st.session_state['prompt'] = st.chat_input(placeholder="Would you like to know anything else?", disabled=False)
+    if st.session_state['prompt']:
+        prompt_visa_info()
+
+for idx, msg in enumerate(st.session_state['user']["messages"]):
+    if idx > 0:
+        messages_length = len(st.session_state['user']["messages"])
+        if msg["role"] == "assistant":
+            if idx < messages_length - 1 or idx == 0:
+                st.chat_message(msg["role"], avatar="app/VisaInfo-chat-logo.png").write(msg["content"])
+            else:
+                if idx == 2:
+                    time.sleep(1.69)
+                def stream_data():
+                    for letter in list(msg["content"]):
+                        yield letter
+                        time.sleep(0.0069)
+                with st.chat_message(msg["role"], avatar="app/VisaInfo-chat-logo.png"):
+                    st.write_stream(stream_data)
+            if idx == 2:
+                @st.fragment
+                def thumbs_up():
+                    thumbs_up = st.feedback("thumbs")
+                    if thumbs_up != None:
+                        if thumbs_up:
+                            st.toast("Thanks for the positive feedback, we hope VisaInfo.chat has made it easier for you to find your visa!")
+                        else:
+                            st.toast("Thank you for the negative feedback and actively making VisaInfo.chat more accurate.")
+                        requests.post(os.environ["VISAINFO_ENDPOINT"]+"feedback", json = {
+                            "feedback_id": st.session_state['user']["messages"][idx]["feedback_id"],
+                            "thumbs_up": thumbs_up,
+                            "sess_id": st.session_state['user']["user_id"]
+                        })
+                thumbs_up()
+        else:
+                st.chat_message(msg["role"]).write(msg["content"])
+
+if st.session_state['state'] == 0:
+    st.session_state['destination'] = st.selectbox("Destination", index=None, placeholder="Pick a country", options=sorted([entry["country"] for entry in st.session_state['countryData']["countries"]]))
+    st.session_state['passport'] = st.selectbox("Passport", index=None, placeholder="Pick a passport", options=sorted([entry["passport"] for entry in st.session_state['countryData']["countries"] if entry["country"] != st.session_state['destination']]))
+    if st.session_state['destination'] == None or st.session_state['passport'] == None:
+        st.button("Generate Visa Info", on_click=generate_visa_info, disabled=True, type="secondary")
+    else:
+        st.button("Generate Visa Info", on_click=generate_visa_info, disabled=False, type="primary", key="Generate")
+    st.markdown("""
+    <style>
+        [data-testid="stChatInput"] {
+            visibility: hidden;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+    st.session_state['prompt'] = st.chat_input(placeholder="Would you like to know anything else?", disabled=True)
